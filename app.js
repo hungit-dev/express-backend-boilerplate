ad7c262fbf46988239807041ff114a6e89955c7c
Hungitdev <hungitdev@gmail.com>
2025-10-08 15:42:15 -0600

    add indexRouter

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const assetsPath = path.join(__dirname, "public");
-const usersRouter = require("./routes/usersRouter");
+const indexRouter = require("./routes/indexRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
-app.use("/", usersRouter);
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
